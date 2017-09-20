08cfea2e4f9abc4ccd8de83e2e430498b7b1df28
imenBHamed <imen.benhamed123@gmail.com>
2017-09-20 19:58:49 -0400

    ajout notations JPA

--- src/main/java/com/app/entities/IR.java
+++ src/main/java/com/app/entities/IR.java
@@ -1,5 +1,22 @@
 package com.app.entities;
 
-public class IR {
+import java.util.Date;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+@Entity
+@DiscriminatorValue("IR")
+public class IR extends Taxe{
+
+	public IR() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public IR(String titre, Date dateTaxe, double montant, Entreprise entreprise) {
+		super(titre, dateTaxe, montant, entreprise);
+		// TODO Auto-generated constructor stub
+	}
+	
 
 }
--- src/main/java/com/app/entities/TVA.java
+++ src/main/java/com/app/entities/TVA.java
@@ -1,5 +1,23 @@
 package com.app.entities;
 
-public class TVA {
+import java.util.Date;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+@Entity
+@DiscriminatorValue("TVA")
+public class TVA extends Taxe{
+
+	public TVA() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public TVA(String titre, Date dateTaxe, double montant,
+			Entreprise entreprise) {
+		super(titre, dateTaxe, montant, entreprise);
+		// TODO Auto-generated constructor stub
+	}
+	
 
 }
