944a36d31a8cba1d7c7f417a6c2b17c4eb9ad4e3
imenBHamed <imen.benhamed123@gmail.com>
2017-09-28 21:00:22 -0400

    test page web

--- src/main/java/com/example/demo/AppTaxesApplication.java
+++ src/main/java/com/example/demo/AppTaxesApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.app.dao.EntrepriseRepository;
