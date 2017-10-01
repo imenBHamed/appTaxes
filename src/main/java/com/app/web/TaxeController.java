1e624000a6f6054b6c19139aecb33a1e86c2cb95
imenBHamed <imen.benhamed123@gmail.com>
2017-09-30 23:49:56 -0400

    ajout vue entreprise.html

--- src/main/java/com/app/web/TaxeController.java
+++ src/main/java/com/app/web/TaxeController.java
@@ -1,11 +1,17 @@
 package com.app.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.app.dao.EntrepriseRepository;
+import com.app.entities.Entreprise;
 
 @Controller
 public class TaxeController {
@@ -14,7 +20,10 @@ public class TaxeController {
 	private EntrepriseRepository entrepriseRepository;
 	
 	@RequestMapping(value="/entreprises", method=RequestMethod.GET)
-	public String index(){
+	
+	public String index(Model model){
+		List<Entreprise> listEntreprises= entrepriseRepository.findAll();
+		model.addAttribute("listEntreprises", listEntreprises);
 		return "entreprises";
 	}
 	
--- src/main/java/com/example/demo/AppTaxesApplication.java
+++ src/main/java/com/example/demo/AppTaxesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.app.dao.EntrepriseRepository;
@@ -16,6 +17,7 @@ import com.app.entities.IR;
 import com.app.entities.TVA;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.app.web"})
 @EntityScan("com.app.entities")
 @EnableJpaRepositories("com.app.dao")
 public class AppTaxesApplication implements CommandLineRunner {
