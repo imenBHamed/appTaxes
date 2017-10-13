1257aeb5eefaf789649995b236f4d376255de326
imenBHamed <imen.benhamed123@gmail.com>
2017-10-12 21:27:14 -0400

    ajout action rechercher

--- src/main/java/com/app/web/TaxeController.java
+++ src/main/java/com/app/web/TaxeController.java
@@ -61,6 +61,7 @@ public class TaxeController {
 	public String save(Model model, Long code) {
 		Entreprise e= new Entreprise();
 		e.setCode(code);
+		model.addAttribute("entreprises", entrepriseRepository.findAll());
 	 model.addAttribute("taxes", taxesRepository.findByEntreprise(e));
 		return "taxes";
 	}
