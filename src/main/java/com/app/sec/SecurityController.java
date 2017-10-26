3c4976783842f1b97a1fe6a78719bc486e4b720b
imenBHamed <imen.benhamed123@gmail.com>
2017-10-25 21:26:05 -0400

    ajout securityController

---
+++ src/main/java/com/app/sec/SecurityController.java
@@ -0,0 +1,22 @@
+package com.app.sec;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class SecurityController {
+	@RequestMapping("/login")
+		public String login(){
+			return "login";
+		}
+	
+	@RequestMapping("/")
+	public String home(){
+		return "redirect:/entreprises";
+	}
+	
+	@RequestMapping("/403")
+	public String accessDenied(){
+		return "403";
+	}
+	
+
+}
--- src/main/java/com/example/demo/AppTaxesApplication.java
+++ src/main/java/com/example/demo/AppTaxesApplication.java
@@ -5,12 +5,9 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.app.dao.EntrepriseRepository;
@@ -19,7 +16,7 @@ import com.app.entities.Entreprise;
 import com.app.entities.IR;
 import com.app.entities.TVA;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication
 @ComponentScan(basePackages={"com.app.web", "com.app.sec"})
 @EntityScan("com.app.entities")
 @EnableJpaRepositories("com.app.dao")
